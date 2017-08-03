2958a5636149fbdd2e585c97d317813f5e9787f0
Armin Schlegel <Armin.Schlegel@conplement.de>
2017-08-03 13:28:44 +0200

    Float PID working. Added Unit test

--- sw/embedded_328/src/Bsw/Pid/Pid.c
+++ sw/embedded_328/src/Bsw/Pid/Pid.c
@@ -1,28 +1,24 @@
 #include "Std_Types.h"
 #include "Pid.h"
 
-void Pid_Init(Pid_DataType *pid)
+void Pid_Init(Pid_DataType *pid, float _SampleRateMs, float Min, float Max)
 {
-    pid->Kp = 0.0f;
+    pid->Kp = 1.0f;
     pid->Ki = 0.0f;
     pid->Kd = 0.0f;
     pid->Error = 0.0f;
     pid->ErrorOld = 0.0f;
     pid->ErrorSum = 0.0f;
-    pid->SampleTimeMs = 0.0f;
+    pid->SampleTimeMs = _SampleRateMs;
     pid->Setpoint = 0.0;
-    pid->MaxLimit = 100.0f;
-    pid->MinLimit = 0.0f;
+    pid->MaxLimit = Max;
+    pid->MinLimit = Min;
+    pid->CalcI = 0.0;
 }
 
 void Pid_SetSetpoint(Pid_DataType *pid, float setpoint)
 {
     pid->Setpoint = setpoint;
-}
-
-void Pid_SetSampleRateMs(Pid_DataType *pid, float _SampleRateMs)
-{
-    pid->SampleTimeMs = _SampleRateMs;
 }
 
 void Pid_SetParameters(Pid_DataType *pid, float _p, float _i, float _d)
@@ -32,21 +28,22 @@ void Pid_SetParameters(Pid_DataType *pid, float _p, float _i, float _d)
     pid->Kd = _d / pid->SampleTimeMs;
 }
 
-void Pid_Handler(Pid_DataType *pid, float CurrentValue)
+float Pid_Handler(Pid_DataType *pid)
 {
-    float CalcI = 0.0;
-
-    pid->Error = pid->Setpoint - CurrentValue;
-    CalcI += (CalcI * pid->Error);
-    if(CalcI > pid->MaxLimit)
+    float input = pid->Output;
+    pid->Error = pid->Setpoint - input;
+    pid->CalcI += (pid->Ki * pid->Error);
+    if(pid->CalcI > pid->MaxLimit)
     {
-        CalcI = pid->MaxLimit;
+        pid->CalcI = pid->MaxLimit;
     }
-    else if(CalcI < pid->MinLimit)
+    else if(pid->CalcI < pid->MinLimit)
     {
-        CalcI = pid->MinLimit;
+        pid->CalcI = pid->MinLimit;
     }
-    pid->Output = pid->Kp * pid->Error + CalcI - pid->Kd * (CurrentValue - pid->LastInput);
+    pid->Output = pid->Kp * pid->Error;
+    pid->Output += pid->CalcI;
+    pid->Output -= pid->Kd * (input - pid->LastInput);
 
     if(pid->Output > pid->MaxLimit)
     {
@@ -57,7 +54,8 @@ void Pid_Handler(Pid_DataType *pid, float CurrentValue)
         pid->Output = pid->MinLimit;
     }
 
-    pid->LastInput = CurrentValue;
+    pid->LastInput = input;
+    return pid->Output;
 }
 
 float Pid_GetOutput(Pid_DataType *pid)
--- sw/embedded_328/src/Bsw/Pid/Pid.h
+++ sw/embedded_328/src/Bsw/Pid/Pid.h
@@ -9,17 +9,19 @@ typedef struct
     float Kp;
     float Ki;
     float Kd;
-    float Error;
     float ErrorSum;
+    float Error;
     float ErrorOld;
     float Output;
     float Setpoint;
     float MaxLimit;
     float MinLimit;
     float LastInput;
+    float CalcI;
 } Pid_DataType;
 
-void Pid_Init(Pid_DataType *pid);
+void Pid_Init(Pid_DataType *pid, float _SampleRateMs, float Min, float Max);
+float Pid_Handler(Pid_DataType *pid);
 void Pid_SetParameters(Pid_DataType *pid, float _p, float _i, float _d);
 void Pid_SetSetpoint(Pid_DataType *pid, float setpoint);
 float Pid_GetOutput(Pid_DataType *pid);
